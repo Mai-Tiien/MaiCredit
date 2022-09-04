d62560ffe356a1a39c97029c64df0f8b99b3559f
Mai Tiien <62517731+Mai-Tiien@users.noreply.github.com>
2022-09-04 23:07:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def help_command(message):
     
 @bot.message_handler(commands=['whoiam'])
 def help_test(message):
-    bot.send_message(message.chat.id, "Group: {test}\nFirst name: {test1}\nTitle: {test2}".format(test=message.chat.username, test1=message.from_user.first_name, test2=message.chat.title))     
+    bot.send_message(message.chat.id, "Group: {test}\nFirst name: {test1}\nTitle: {test2}".format(test=message.chat.username, test1=message.from_user.first_name, test2=emp+emp_type))     
       
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def help_command(message):
@@ -34,6 +34,37 @@ def help_command(message):
     elif message.text == 'Май Геній' or 'Май геній' or 'Слава Україні':
             bot.reply_to(message, "Вітаю *{name}*, ти отримуэш від вождя плюс {num} МайКредіт".format(name = message.from_user.first_name, num=random.randint(15, 80)), parse_mode="Markdown") 
 
+emp = ['Хохол', 'Байрактар', 'Байдон', 'Зєлєбобік', 'Окраинец', 'Броварска макака', 'Тернопівска хвойда', 
+       'Порохобот', 'Салоїд', 'Майданутий', 'Байрактар', 'Русофоб', 'Натовец', 'Джавелін', 'Мазепонець', 'Кіця', 'Пес']
+emp_text = [
+    ', що намагається воскресить Бандеру',
+    ', що розробляє нову біологічну зброю',
+    ', що ущемля рускоговарящего',
+    ', що стріляє в Кубань',
+    ', що змусив Пушилина з ДНР сісти на бутилку',
+    ', що пічкає ядеркою птахів з Херосонського заповідника',
+    ', що копає одеське метро',
+    ', що погрожує ядеркою Росії',
+    ', що викопав Чорне море',
+    ', що особисто з Леніним писав Енеїду в 1917 році',
+    ', що донбить бомбас-бімбас',
+    ', що кожен ранок спалює російські прапори у львівських школах',
+    ', що розбомбив Антонівський міст',
+    ', що їсть російських немовлят на сніданок',
+    ', що пришиває свастику на форму ЗСУ',
+    ', що підірвав автівку Дугіни',
+    ', що створив бандеро мобіль',
+    ', що розробляє нано-екзоскелет у Броварській нанолабораторії',
+    ', що розшатує санкцією проти Росії',
+    ', що їбе путліра',
+    ', що їбаше сіль',
+    ', що ходить в публичну качалку',
+    ', що дивиться Гей порно на українському перекладі',
+    ', що підпалює петарди',
+    ', що лайкає базовані українські меми'
+]
+emp_list = emp[random.randint(0, 16)]
+emp_type = emp_text[random.randint(0, 25)]
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
@@ -50,4 +81,3 @@ def webhook():
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    
