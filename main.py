1af12f058d27596ed66adcaa6f79d659f25d9fd2
Mai Tiien <62517731+Mai-Tiien@users.noreply.github.com>
2022-09-04 21:06:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import telebot
-import time
 import random
+import os
+import time
+from flask import Flask, request
 
+APP_NAME='https://maicredit.herokuapp.com/'
 TOKEN = '5769001050:AAE6tjmtU0diovOjnf7rqGQ4p-oPxbGFj14'
 bot = telebot.TeleBot(TOKEN)
+bot.can_join_groups = True
+
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def help_command(message):
@@ -23,21 +29,21 @@ def help_command(message):
             bot.reply_to(message, "Нажаль *{name}*, твій рейтинг впав на -{num} соціальних балів МайКредіт".format(name = message.chat.first_name, num=random.randint(15, 100)), parse_mode="Markdown")     
     elif message.text == 'Май Геній' or 'Май геній' or 'Слава Україні':
             bot.reply_to(message, "Вітаю *{name}*, ти отримуэш від вождя плюс {num} МайКредіт".format(name = message.chat.first_name, num=random.randint(15, 80)), parse_mode="Markdown") 
-  
 
-@bot.message_handler(func=lambda message: True, content_types=['photo'])
-def echo_photo(message):
-    img = message.photo[2].file_id
-    bot.send_message(560831108, "Запит від @{name} десь там 👇".format(name=message.chat.username), parse_mode="Markdown")
-    bot.send_photo(560831108, img, message.caption)
 
-@bot.message_handler(func=lambda message: True, content_types=['video', 'video_note'])
-def echo_video(message):
-    bot.send_message(560831108, "Відправка відео від @{name} десь там 👇".format(name=message.chat.username), parse_mode="Markdown")
-    bot.send_video(560831108, message.video.file_id, timeout=10)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "Hello, world!", 200
 
-while True:
-    try:
-        bot.polling(none_stop=True, interval=0, timeout=0)
-    except:
-        time.sleep(10)
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_NAME + TOKEN)
+    return "Hello, world!", 200    
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    
