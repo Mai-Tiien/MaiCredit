fb11f6ae5cd287e4cd7a37aaf565ab4b227ae4b9
Mai Tiien <62517731+Mai-Tiien@users.noreply.github.com>
2022-09-05 22:10:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -72,12 +72,12 @@ def help_test(message):
       
 @bot.message_handler(commands=['maicredit'])
 def maicredit_command(message):
-    rn = random.randint(1,10) 
-    if rn > 0:
+    rn = random.randint(1,3) 
+    if rn >= 1:
         bot.reply_to(message, "Вітаю *{name}*, твій рейтинг піднявся на +{num} МайКредіт".format(name = message.from_user.first_name, num=random.randint(15, 200)), parse_mode="Markdown") 
-    elif rn > 5:    
+    if rn <= 2:    
         bot.reply_to(message, "Нажаль *{name}*, твій рейтинг впав на -{num} МайКредіт".format(name = message.from_user.first_name, num=random.randint(15, 100)), parse_mode="Markdown") 
-    elif rn < 10:  
+    if rn == 3:  
         bot.reply_to(message, "*{name}*, ти розчарувати великий вождь! Святослав зробить пуля тобі в лоб вогонь! Ти втратив -{num} МайКредіт".format(name = message.from_user.first_name, num=random.randint(15, 100)), parse_mode="Markdown")
 
 @server.route('/' + TOKEN, methods=['POST'])
